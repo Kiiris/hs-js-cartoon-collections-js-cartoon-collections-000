95f037c5ed8b337c391a1a335e9bff55cdb06fb8
Calvin Menyfield <barthlemont@gmail.com>
2019-12-19 19:02:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function dwarfRollCall(dwarves) {
+  var Mystring= []
+  let i=0
+  for (let i=0; i<dwarves.length; i++) {
+    Mystring.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return Mystring.join('')
+
+
+}
+function summonCaptainPlanet(planeteerCalls){ for (let i = 0; i < planeteerCalls.length; i++) {
+ planeteerCalls[i] = planeteerCalls[i].toUpperCase();}  for(var i=0; i < planeteerCalls.length;i++){
+    planeteerCalls[i]= planeteerCalls[i] + "!";} return planeteerCalls
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function longPlaneteerCalls(words) {if (words.length < 4){ return false} if (words.length > 4) {return true}
 }
 
-function longPlaneteerCalls(words) {
-}
+function findTheCheese (foods) { var cheese = ["cheddar", "gouda", "camembert"];
+  var cheddarCheese = ["banana", "cheddar", "sock"];
+     for(var i = 0; i < foods.length; i++) {
+      if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") { 
+    return cheddarCheese[i];
+  }
+     }
+ if (foods[i]!== "cheddar"); (foods[i] !== "gouda"); (foods[i] !== "camembert"); {
+   return "no cheese!";
+ }
 
-function findTheCheese (foods) {
 }
