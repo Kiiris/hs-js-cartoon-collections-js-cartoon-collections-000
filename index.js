9d493120fee27e7bba89c41b0955f7c8afcea5c6
Calvin Menyfield <barthlemont@gmail.com>
2020-04-27 02:02:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function dwarfRollCall(dwarves) {
   var Mystring= []
-  let i=0
   for (let i=0; i<dwarves.length; i++) {
     Mystring.push(`${i+1}. ${dwarves[i]} `)
   }
@@ -13,18 +12,23 @@ function summonCaptainPlanet(planeteerCalls){ for (let i = 0; i < planeteerCalls
     planeteerCalls[i]= planeteerCalls[i] + "!";} return planeteerCalls
 }
 
-function longPlaneteerCalls(words) {if (words.length < 4){ return false} if (words.length > 4) {return true}
+function longPlaneteerCalls(words) {
+  if(words.length < 4){ 
+  return false} if (words.length > 4) {return true}
 }
 
-function findTheCheese (foods) { var cheese = ["cheddar", "gouda", "camembert"];
-  var cheddarCheese = ["banana", "cheddar", "sock"];
-     for(var i = 0; i < foods.length; i++) {
-      if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") { 
-    return cheddarCheese[i];
-  }
+ function findTheCheese (foods) { 
+   var cheese = ["cheddar", "gouda", "camembert", "swiss cheese"];
+     for(var i = 0; i< foods.length; i++){
+       for(var p = 0; p< cheese.length; p++){
+          if (foods[i] === cheese[p]){ 
+            return foods[i]
+            }
+        }
      }
- if (foods[i]!== "cheddar"); (foods[i] !== "gouda"); (foods[i] !== "camembert"); {
-   return "no cheese!";
- }
-
-}
+            return "no cheese!";
+    }
+function wordswithBe(words) { var b = ["B", "b"];
+  for (var i = 0; i < words.length; i++){
+    if (words[i] === b[i]){
+    return words[i]
